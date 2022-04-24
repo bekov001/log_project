d1e4a164e25fde3cb4bbd5d1acfd4fbc5415d69e
Anuar <ava200ava@gmail.com>
2022-04-25 00:17:38 +0600

    создание регистрации и авторизации, аутенфикация

--- src/app.py
+++ src/app.py
@@ -6,16 +6,32 @@ from werkzeug.utils import secure_filename
 from src.api import delivery_api
 from src.data import db_session
 from src.data.products import Products
+from src.data.user import User
 from src.forms.add_code import AddCodeForm
+from src.forms.authentication_form import RegisterForm, LoginForm
 from src.forms.input_form import FindForm
 
+from flask_login import LoginManager, login_user, login_required, logout_user, \
+    current_user
+
+
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/code.db?check_same_thread=False"
+login_manager = LoginManager()
+login_manager.init_app(app)
 # img_path = "static/img/"
 img_path = "/home/anuarka/logistic/static/img/"
 # app.config['RESIZE_URL'] = 'https://mysite.com/'
 # app.config['RESIZE_ROOT'] = '/static/img/'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -32,6 +48,7 @@ def index():
 
 
 @app.route("/add_code", methods=["POST", "GET"])
+@login_required
 def add_code():
     """Добавление кода"""
     form = AddCodeForm()
@@ -58,12 +75,56 @@ def add_code():
     return render_template("add_code.html", form=form)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('authentication/login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('authentication/login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = User()
+        user.email = form.email.data
+        user.name = form.name.data
+        user.surname = form.surname.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        login_user(user, remember=form.remember_me.data)
+        return redirect("/")
+    return render_template('authentication/register.html', title='Авторизация', form=form)
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('profile.html')
+
+
 def main():
     """Главная функция запуска"""
-    db_session.global_init("src/db/code.db")
+    db_session.global_init("db/code.db")
     app.register_blueprint(delivery_api.blueprint)
     app.debug = True
     app.run(host="0.0.0.0", port=5000)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 if __name__ == '__main__':
