7337f88a307fa9af4bc32fc4cf445a56b130f50c
Anuar <ava200ava@gmail.com>
2022-05-05 17:21:55 +0600

    Untrack database

--- src/app.py
+++ src/app.py
@@ -118,7 +118,7 @@ def register():
 
 def main():
     """Главная функция запуска"""
-    db_session.global_init("src/db/log.db")
+    db_session.global_init("src/db/db.db")
     app.register_blueprint(delivery_api.blueprint)
     app.register_blueprint(product_api.blueprint)
     app.register_blueprint(user_api.blueprint)
